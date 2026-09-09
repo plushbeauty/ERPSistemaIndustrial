1fdc218362a2c81b8502c6a7f8a7729b9ed9c347
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-09 20:02:43 -0300

    feat: carregar novo design do ERP industrial

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles/sgq-overrides.css'
 import './styles/public-industrial.css'
 import './styles/industrial-enhancements.css'
 import './styles/public-home-v2.css'
+import './styles/industrial-v2.css'
 
 if('serviceWorker' in navigator) window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>undefined))
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppEntry /></React.StrictMode>)
