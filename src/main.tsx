660409bbc8ad73f8f574ec4ba6ef002bc8d7b0bb
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-09 16:36:37 -0300

    style: carregar camada visual SGQ ERP

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppIndustrial from './AppIndustrial'
 import './styles/index.css'
+import './styles/sgq-overrides.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppIndustrial/></React.StrictMode>)
