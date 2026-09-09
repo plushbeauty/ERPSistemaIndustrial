48667c4eb389056371a60072fcc9a9afeb209038
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-09 19:41:22 -0300

    feat: registrar service worker do PWA

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,5 @@ import './styles/public-industrial.css'
 import './styles/industrial-enhancements.css'
 import './styles/public-home-v2.css'
 
+if('serviceWorker' in navigator) window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>undefined))
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppEntry /></React.StrictMode>)
