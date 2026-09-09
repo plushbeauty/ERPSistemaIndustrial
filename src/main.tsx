42dd3584366878efa357a30c5a1d4b19ccda8aa9
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-09 15:15:49 -0300

    feat: ativar nova aplicação industrial funcional

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import { AuthProvider } from './auth/AuthProvider'
+import AppIndustrial from './AppIndustrial'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><AuthProvider><App /></AuthProvider></React.StrictMode>,
+  <React.StrictMode><AppIndustrial /></React.StrictMode>,
 )
