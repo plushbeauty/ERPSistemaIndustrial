566bf759a055d8e0c847899353bb34028d931da4
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-09 15:16:31 -0300

    fix: usar aplicação industrial principal com agenda PCP

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppIndustrial from './AppIndustrial'
 import './styles/index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><AppIndustrial /></React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppIndustrial/></React.StrictMode>)
