72946feb6f2e79dbf094b2db7482f90610cd66a0
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-10 11:07:16 -0300

    feat: incluir estilos do painel master

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './styles/industrial-enhancements.css'
 import './styles/public-home-v2.css'
 import './styles/industrial-v2.css'
 import './styles/fiscal-public.css'
+import './styles/master.css'
 
 if('serviceWorker' in navigator) window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>undefined))
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppEntry /></React.StrictMode>)
