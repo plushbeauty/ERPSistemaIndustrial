c89d0ddb5e85ae572fd6feeda5e78e5396ec566c
Fernando Schuinsekel <plushbeauty.sistema@gmail.com>
2026-09-20 14:09:19 -0300

    fix: corrigir DEMO e bloquear ações de escrita

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    assetsDir: 'assets',
+    sourcemap: false,
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 2000,
   },
-})
+});
